d64c2df3c297d52c4d08e49fb540249f3feb8beb
Jorge Hernandez <jorge09ha@gmail.com>
2023-03-05 13:49:20 -0600

    Repo
    Se Crea repo.

---
+++ BikeLab/src/test/java/com/BikeLab/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikeLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
