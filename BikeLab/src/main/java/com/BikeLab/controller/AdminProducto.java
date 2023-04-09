fdc056a6e8b5a96b36f700fa5eefe516604b8c23
Jorge Hernandez <jorge09ha@gmail.com>
2023-04-09 00:15:28 -0600

    CRUD finales admin

--- BikeLab/src/main/java/com/BikeLab/controller/AdminProducto.java
+++ BikeLab/src/main/java/com/BikeLab/controller/AdminProducto.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -52,11 +55,129 @@ public class AdminProducto {
         model.addAttribute("tipos", lista);
         return "adm_VerTipoP";
     }
+
     //-------------------------- New --------------------------
+    @GetMapping("/admin/marca/nuevo")
+    public String crearMarca(Model model) {
+        Marca marca = new Marca();
+        model.addAttribute("titulo", "Marca Nueva");
+        model.addAttribute("marca", marca);
+        return "adm_crearMarcaP";
+    }
+
+    @GetMapping("/admin/familia/nuevo")
+    public String crearFamiliaP(Model model) {
+        FamiliaProducto familiaProducto = new FamiliaProducto();
+        model.addAttribute("titulo", "Nueva Familia Producto");
+        model.addAttribute("familia", familiaProducto);
+        return "adm_crearFamiliaP";
+    }
+
+    @GetMapping("/admin/tipo/nuevo")
+    public String crearTipoP(Model model) {
+         List<FamiliaProducto> lista = familiaProductoService.getAllFamiliaProducto();
+        TipoProducto tipoProducto = new TipoProducto();
+        model.addAttribute("titulo", "Nueva Tipo Producto");
+        model.addAttribute("tipo", tipoProducto);
+        model.addAttribute("familiaproducto", lista);
+        return "adm_crearTipoP";
+    }
+
     //-------------------------- Save --------------------------
+    @PostMapping("/save/marca")
+    public String guardarMarca(@ModelAttribute("Marca") Marca marca) {
+        marcaService.saveMarca(marca);
+        return "redirect:/admin/marca_producto";
+    }
+
+    @PostMapping("/save/familia")
+    public String guardarFamiliaP(@ModelAttribute("Familia") FamiliaProducto familiaProducto) {
+        familiaProductoService.saveFamiliaProducto(familiaProducto);
+        return "redirect:/admin/familia_producto";
+    }
+
+    @PostMapping("/save/tipo")
+    public String guardarTipoP(@ModelAttribute("Tipo") TipoProducto tipoProducto) {
+        tipoProductoService.saveTipoProducto(tipoProducto);
+        return "redirect:/admin/tipo_producto";
+    }
+
     //-------------------------- Delete --------------------------
+    @GetMapping("/eliminar/marca/{id}")
+    public String eliminarMarcaP(@PathVariable Long id) {
+        marcaService.deleteMarca(id);
+        return "redirect:/admin/marca_producto";
+    }
+
+    @GetMapping("/eliminar/familia/{id}")
+    public String eliminarFamiliaP(@PathVariable Long id) {
+        familiaProductoService.deleteFamiliaProducto(id);
+        return "redirect:/admin/familia_producto";
+    }
+
+    @GetMapping("/eliminar/tipo/{id}")
+    public String eliminarTipoP(@PathVariable Long id) {
+        tipoProductoService.deleteTipoProducto(id);
+        return "redirect:/admin/tipo_producto";
+    }
+
     //-------------------------- UpDate --------------------------
     //*Marca
+    @GetMapping("/editar/marca/{id}")
+    public String editarMarcaP(@PathVariable("id") Long id, Model model) {
+        Marca a = marcaService.getMarcaById(id);
+        model.addAttribute("titulo", "Editar Marca");
+        model.addAttribute("marca", a);
+        return "adm_editarMarcaP";
+    }
+
+    @PostMapping("/editar/marca/{id}")
+    public String actualizarMarcaP(@PathVariable Long id, @ModelAttribute("Marca") Marca marca) {
+        Marca editar = marcaService.getMarcaById(id);
+        editar.setId(id);
+        editar.setNombre(marca.getNombre());
+        marcaService.saveMarca(editar);
+        return "redirect:/admin/marca_producto";
+    }
+
     //*Familia
+    @GetMapping("/editar/familia/{id}")
+    public String editarFamiliaP(@PathVariable("id") Long id, Model model) {
+        FamiliaProducto a = familiaProductoService.getFamiliaProductoById(id);
+        model.addAttribute("titulo", "Editar Familia Producto");
+        model.addAttribute("familia", a);
+        return "adm_editarFamiliaP";
+    }
+
+    @PostMapping("/editar/familia/{id}")
+    public String actualizarFamiliaP(@PathVariable Long id, @ModelAttribute("Familia") FamiliaProducto familiaP) {
+        FamiliaProducto editar = familiaProductoService.getFamiliaProductoById(id);
+        editar.setId(id);
+        editar.setFamilia(familiaP.getFamilia());
+        editar.setDetalle(familiaP.getDetalle());
+        familiaProductoService.saveFamiliaProducto(editar);
+        return "redirect:/admin/familia_producto";
+    }
+
     //*Tipo
+    @GetMapping("/editar/tipo/{id}")
+    public String editarTipoP(@PathVariable("id") Long id, Model model) {
+        TipoProducto a = tipoProductoService.getTipoProductoById(id);
+        List<FamiliaProducto> lista = familiaProductoService.getAllFamiliaProducto();
+        model.addAttribute("titulo", "Editar Tipo Producto");
+        model.addAttribute("tipo", a);
+        model.addAttribute("familiaproducto", lista);
+        return "adm_editarTipoP";
+    }
+
+    @PostMapping("/editar/tipo/{id}")
+    public String actualizarTipoP(@PathVariable Long id, @ModelAttribute("Tipo") TipoProducto tipoProducto) {
+        TipoProducto editar = tipoProductoService.getTipoProductoById(id);
+        editar.setId(id);
+        editar.setTipo(tipoProducto.getTipo());
+        editar.setDetalle(tipoProducto.getDetalle());
+        editar.setFamiliaproducto(tipoProducto.getFamiliaproducto());
+        tipoProductoService.saveTipoProducto(editar);
+        return "redirect:/admin/tipo_producto";
+    }
 }
--- BikeLab/src/main/java/com/BikeLab/controller/AdminProveedor.java
+++ BikeLab/src/main/java/com/BikeLab/controller/AdminProveedor.java
@@ -1,12 +1,16 @@
 package com.BikeLab.controller;
 
 import com.BikeLab.entity.Proveedor;
+import com.BikeLab.entity.Rol;
 import com.BikeLab.service.IProveedorService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -35,7 +39,36 @@ public class AdminProveedor {
         return "adm_crearProveedor";
     }
     //-------------------------- Save --------------------------
+        @PostMapping("/save/proveedor")
+    public String guardarRole(@ModelAttribute("proveedor") Proveedor proveedor) {
+        proveedorService.saveProveedor(proveedor);
+        return "redirect:/admin/proveedor";
+    }
     //-------------------------- Delete --------------------------
+        @GetMapping("/eliminar/proveedor/{id}")
+    public String eliminarRole(@PathVariable Long id) {
+        proveedorService.deleteProveedor(id);
+        return "redirect:/admin/proveedor";
+    }
     //-------------------------- UpDate --------------------------
+        //*Proveedores
+    @GetMapping("/editar/proveedor/{id}")
+    public String editarRol(@PathVariable("id") Long id, Model model) {
+        Proveedor a = proveedorService.getProveedorById(id);
+        model.addAttribute("titulo", "Editar Proveedor");
+        model.addAttribute("proveedor", a);
+        return "adm_editarProveedor";
+    }
+
+    @PostMapping("/editar/proveedor/{id}")
+    public String actualizarRol(@PathVariable Long id, @ModelAttribute("Rol") Proveedor proveedor) {
+        Proveedor editar = proveedorService.getProveedorById(id);
+        editar.setId(id);
+        editar.setNombre(proveedor.getNombre());
+        editar.setCorreo(proveedor.getCorreo());
+        editar.setTelefono(proveedor.getTelefono());
+        proveedorService.saveProveedor(editar);
+        return "redirect:/admin/proveedor";
+    }
 
 }
